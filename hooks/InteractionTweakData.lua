0b509c52be6aabb12ade1031fb54af03ceb38a03
VxWolf <vxwolf368@gmail.com>
2020-11-05 18:17:47 +1100

    that's it we startin from scratch
    Implemented:
    Basic Faction Enemies (lights, heavies, all specials except captain and turret)
    Automatic Respawning Toggle
    Switching between Factions

--- hooks/InteractionTweakData.lua
+++
@@ -1,7 +0,0 @@
-Hooks:PostHook(InteractionTweakData, "init", "enemy_spawner_interactions", function(self, tweak_data)
-	self.push_button = {
-		text_id = "hud_int_push_button",
-		sound_done = "button_push",
-		axis = "y"
-	}
-end)
--- hooks/customsafehousemanager.lua
+++
@@ -1,7 +0,0 @@
-function CustomSafehouseManager:award(id) --Disable trophies
-	return
-end
-
-function CustomSafehouseManager:award_progress(id, amount) --Disable trophy progress
-	return
-end
