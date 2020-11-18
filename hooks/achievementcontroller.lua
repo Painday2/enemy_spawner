3bc85ab9733b4cbf04b12ef74c50312b34ceb99f
Cupcake <meowcats2121@gmail.com>
2020-11-18 10:02:59 -0700

    Fixed achievements, added new guns from last update, added greenscreen, added icon

--- hooks/achievementcontroller.lua
+++ hooks/achievementcontroller.lua
@@ -1,23 +1,39 @@
---bonk mako did this trash
 Hooks:PostHook(ElementBLCustomAchievement, "init", "init_prov_grnd_package", function (self)
 CustomAchievementPackage:init("prov_grnd_package")
 end)
 
-Hooks:PostHook(ElementBLCustomAchievement, "on_executed", "init_prov_grnd_achievements", function (self)
-		if CustomAchievement:IsUnlocked(prov_grnd_50k) == true then
-			local element = managers.mission:get_mission_element(101470)
-			element:on_executed()
-		end
-		if CustomAchievement:IsUnlocked(prov_grnd_100k) == true then
-			local element = managers.mission:get_mission_element(101473)
-			element:on_executed()
-		end
-		if CustomAchievement:IsUnlocked(prov_grnd_cookie) == true then
-			local element = managers.mission:get_mission_element(101471)
-			element:on_executed()
-		end
-		if CustomAchievement:IsUnlocked(prov_grnd_range) == true then
-			local element = managers.mission:get_mission_element(101472)
-			element:on_executed()
-		end
+local function CheckUnlocked(ach_id, package_id)
+            local config = CustomAchievementPackage:GetConfigOf(ach_id)
+
+            if type(config) == "table" then
+                local achievement = CustomAchievement:new(config, package_id)
+
+                if achievement:IsUnlocked() then
+                    return true
+                end
+            end
+        end
+
+
+
+Hooks:PostHook(ElementDangerZone, "on_executed", "init_prov_grnd_achievements", function (self)
+        if CheckUnlocked("prov_grnd_50k", "prov_grnd_package") == true then
+            local element = managers.mission:get_mission_element_by_name("enable_unlock_kills")
+            element:on_executed()
+        end
+		
+		if CheckUnlocked("prov_grnd_100k", "prov_grnd_package") == true then
+            local element = managers.mission:get_mission_element_by_name("enable_unlock_all")
+            element:on_executed()
+        end
+		
+		if CheckUnlocked("prov_grnd_range", "prov_grnd_package") == true then
+            local element = managers.mission:get_mission_element_by_name("enable_unlock_range")
+            element:on_executed()
+        end
+		
+		if CheckUnlocked("prov_grnd_cookie", "prov_grnd_package") == true then
+            local element = managers.mission:get_mission_element_by_name("enable_unlock_cookie")
+            element:on_executed()
+        end
 end)
